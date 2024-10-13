bc22507a6e92a31df5a11c2882b9425c72596577
Morten Bonderup <moab@eaaa.dk>
2024-10-13 06:54:44 +0200

    Version 13.10.2024

--- src/components/Bestillingskort.jsx
+++ src/components/Bestillingskort.jsx
@@ -1,19 +1,19 @@
 export default function Bestillingskort({ bestilling, handleButton }) {
     return (
         <div style={{ display: "flex", width: "1200px", justifyContent: "space-between" }}>
-            <div>Navn: {bestilling.navn}</div>
-            <div>Email: {bestilling.email}</div>
-            <div>Mobilnr: {bestilling.mobilnummer}</div>
+            <div>Navn: {bestilling.kundenavn}</div>
+            <div>Email: {bestilling.kundeemail}</div>
+            <div>Mobilnr: {bestilling.kundemobil}</div>
             <div>Pizzaer: {bestilling.pizzaer.join(", ")}</div>
             {bestilling.status === "Ny bestilling" ?
                 <div>
                     <span style={{ fontWeight: "bold" }}>{bestilling.status}&nbsp;&nbsp; </span>
-                    <button type="button" data-knap="skiftstatus" value={bestilling.id} onClick={handleButton}>Skift status til "I gang"</button>
+                    <button type="button" data-knap="skiftstatus" value={bestilling.id} onClick={handleButton}>Skift status til &ldquo;I gang&ldquo;</button>
                 </div>
                 :
                 <div>
                     <span style={{ fontWeight: "bold" }}>{bestilling.status} </span>
-                    <button type="button" data-knap="slet" value={bestilling.id} onClick={handleButton}>Slet</button>
+                    <button type="button" data-knap="slet" value={bestilling.id} onClick={handleButton}>Afhentet</button>
                 </div>
             }
         </div>
